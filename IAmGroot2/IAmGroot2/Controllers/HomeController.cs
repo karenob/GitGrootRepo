1204babf03763ec6dd5e3d4f2661d68852291371
Schroedter, Jacqueline <JSchroedter@swbc.com>
2018-08-23 14:09:34 -0500

    Removed code from Home Controller

--- IAmGroot2/IAmGroot2/Controllers/HomeController.cs
+++ IAmGroot2/IAmGroot2/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.Http;
+
 
 namespace IAmGroot2.Controllers
 {
@@ -32,8 +32,8 @@ namespace IAmGroot2.Controllers
         //    return View();
         //}
 
-        [System.Web.Mvc.HttpPost]
-        public ActionResult Contact([FromBody] TestCommand command)
+    
+        public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
